559300cfd1252ac68f48df13664a8b10de014a1b
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2025-06-05 02:28:28 +0700

    500 gen

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import BarcodeGenerator from '../components/BarcodeGenerator';
 import BarcodeTable from '../components/BarcodeTable';
 import TicketGenerator from '../components/TicketGenerator';
+import ImageBasedTicket from '../components/ImageBasedTicket';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <div className="flex flex-row items-center justify-center">
+      {/* <div className="flex flex-row items-center justify-center">
         <BarcodeGenerator />
       </div>
       <div className="flex flex-row items-center justify-center">
@@ -16,6 +17,11 @@ export default function Home() {
       </div>
       <div className="flex flex-row items-center justify-center">
         <TicketGenerator />
+      </div> 
+      */}
+
+<div className="flex flex-row items-center justify-center">
+        <ImageBasedTicket />
       </div>
     </main>
   );
